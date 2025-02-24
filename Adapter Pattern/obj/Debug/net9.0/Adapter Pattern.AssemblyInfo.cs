522218fe35c4983ac5e3fe90edd77aa1e45b8095
Feihuan Peng <ericrealtor1997@gmail.com>
2025-02-23 21:14:02 -0800

    debug

--- Adapter Pattern/obj/Debug/net9.0/Adapter Pattern.AssemblyInfo.cs
+++ Adapter Pattern/obj/Debug/net9.0/Adapter Pattern.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Adapter Pattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1f14bec5dd4298464ae9460e1b27e7fdedd12da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb29458f0dec63f66a1e53815b08509f949258c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Adapter Pattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Adapter Pattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Debug and creating async program/Program.cs
@@ -0,0 +1,28 @@
+﻿
+public class Program
+{
+    public static  async Task PerformLongOperationAsync()
+    {
+
+        try
+        {
+        Console.WriteLine("before start");
+        await Task.Delay(3000);
+        throw new InvalidOperationException("simulate exception");
+        Console.WriteLine("operation complete");
+            
+        }
+        catch (Exception ex)
+        {
+            
+            Console.WriteLine($"Exception: {ex.Message}");
+        }
+
+    }
+
+    public static async Task Main(string[] args)
+    {
+        await PerformLongOperationAsync();
+        Console.WriteLine("main task completed");
+    }
+}
